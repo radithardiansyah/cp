d16af92614ff490b95741042385ddc45c1c3d6df
radit <rdithardiansyah@gmail.com>
2026-06-15 16:04:49 +0700

    problem n the last problem

---
+++ problem_n_the_last_problem.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+    string s;
+    getline(cin, s);
+
+
+    cout << "Thank you, " << s << ", and farewell!";
+
+    return 0;
+}
