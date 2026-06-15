a1220ac11fb6f6b08b8fffd4c48e813b86b65314
radit <rdithardiansyah@gmail.com>
2026-06-15 10:26:56 +0700

    carrots fyuhh

---
+++ kattis_carrots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main(){
+    int contestants;
+    cin >> contestants;
+    int problemSolved;
+    cin >> problemSolved;
+    string describe_contestant;
+    cin >> describe_contestant;
+
+    cout << problemSolved;
+
+
+    return 0;
+}
