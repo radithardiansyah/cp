4236a892ce74ad2a5eedcbd53eff8ed3bd606582
radit <rdithardiansyah@gmail.com>
2026-07-16 16:24:15 +0700

    searching for nessy fyuhh

---
+++ uva-11044.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main(){
+    int test_case;
+    int rows;
+    int s_rows;
+    int columns;
+    int s_columns;
+    cin >> test_case;
+    for(int i = 0; i < test_case; i++){
+cin >> rows >> columns;
+if(rows == 0 || columns == 0){
+    return 0;
+}
+s_rows = (rows - 2)/3;
+s_columns = (columns - 2)/3;
+
+
+cout << s_rows * s_columns << "\n";
+
+    }
+
+    return 0;
+}
