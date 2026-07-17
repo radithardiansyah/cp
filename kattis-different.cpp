9ac29d5827851e1a5fa7d73e3b77a8ae8aaafd9a
radit <rdithardiansyah@gmail.com>
2026-07-17 16:57:10 +0700

    BIG DIFF

---
+++ kattis-different.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include<cmath>
+
+int main(){
+long long i_1;
+long long i_2;
+cin >> i_1 >> i_2;
+
+cout << abs(i_1 - i_2) << "\n";
+
+    return 0;
+}
--- uva-11547.cpp
+++ uva-11547.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 int main(){
 int test_case;
-double n;
-double f_n;
-int f1_n;
+long long n;
 int output;
 
 cin >> test_case;
 
 for(int i = 0; i < test_case; i++){
     cin >> n;
-    f_n = (n * 567)/9 + 7592 * 235;
-    f1_n = (f_n/47) - 498;
 
-    output = (f1_n/10) % 10;
+    n = (n * 567) / 9 + 7492;
+    n = (n * 235) / 47 - 498;
+
+    output = (n/10) % 10;
     cout << output << "\n";
 }
     return 0;
