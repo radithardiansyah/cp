5af31a5ee5ac8434e00e518d43fc0f3c65f05626
radit <rdithardiansyah@gmail.com>
2026-07-18 07:18:55 +0700

    quite fun

--- kattis-different.cpp
+++ kattis-different.cpp
@@ -6,7 +6,8 @@ int main(){
 long long i_1;
 long long i_2;
 cin >> i_1 >> i_2;
-
+//abs(i_1);
+//abs(i_2);
 cout << abs(i_1 - i_2) << "\n";
 
     return 0;
---
+++ kattis-qaly.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+
+int main(){
+int periods;
+cin >> periods;
+double quality_of_life;
+double years;
+double sum = 0.0;
+for(int i = 0; i < periods; i++){
+    cin >> quality_of_life >> years;
+
+    sum += quality_of_life * years;
+}
+cout << fixed << setprecision(6) << sum << "\n";
+
+    return 0;
+}
