17c6e1ceecbfb54aac38795160bb54aadfc5a57d
radit <rdithardiansyah@gmail.com>
2026-07-28 20:54:32 +0700

    ez

---
+++ kattis-help-a-phd.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+int n;
+string n_lines, left, right;
+int op;
+cin >> n;
+for(int i = 0; i < n; i++){
+    cin >> n_lines;
+    if(n_lines == "P=NP"){
+        cout<<"skipped" << "\n";
+    }else{
+         op = n_lines.find("+");
+         left = n_lines.substr(0, op);
+         right = n_lines.substr(op + 1);
+         cout << stoi(left) + stoi(right);
+
+    }
+  
+
+}
+    return 0;
+}
